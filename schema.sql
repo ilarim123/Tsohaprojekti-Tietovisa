487bd8ccd23e062b304056d5b221ca4e70b6c82b
Ilari Matikainen <ilari.matikainen@rokki.net>
2021-04-10 22:26:39 +0300

    edit schema.sql

---
+++ schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    password TEXT,
+    role INTEGER
+);
+
+CREATE TABLE topics (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE questions (
+    id SERIAL PRIMARY KEY,
+    topic_id INTEGER REFERENCES topics,
+    question TEXT,
+    answer TEXT
+);
+
+CREATE TABLE scoreboard (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    topic_id INTEGER REFERENCES topics
+    score INTEGER
+);
+
