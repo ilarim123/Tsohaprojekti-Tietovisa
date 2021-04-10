0dd76ff1b98005c18c8aef996586e0a3df403b82
Ilari Matikainen <ilari.matikainen@rokki.net>
2021-04-10 23:52:14 +0300

    add first lines to .py files

---
+++ users.py
@@ -0,0 +1,12 @@
+from db import db
+from flask import abort, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
+import os
+
+def login(name, password):
+    pass
+
+def logout():
+    pass
+
+
